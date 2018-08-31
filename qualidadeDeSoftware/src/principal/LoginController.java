6c001b2e4c5829e2c01d2369d23def9e49e9cb30
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-08-31 14:26:40 -0300

    Login

---
+++ qualidadeDeSoftware/src/principal/LoginController.java
@@ -0,0 +1,25 @@
+package principal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+
+    @FXML
+    private TextField tfNomeUsuario;
+
+    @FXML
+    private TextField tfSenha;
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    void entrar(ActionEvent event) {
+
+    }
+	
+}
