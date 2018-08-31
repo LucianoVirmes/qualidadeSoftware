73f0601a19eab816275aabdf70416c68996bbac2
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-08-31 13:58:30 -0300

    Adicionando tela menu

--- qualidadeDeSoftware/src/principal/MenuController.java
+++ qualidadeDeSoftware/src/principal/MenuController.java
@@ -1,5 +1,58 @@
 package principal;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
 public class MenuController {
 
+	 @FXML
+	 private MenuItem mmiCadastroUsuario;
+
+    @FXML
+    private MenuItem mmiEditarUsuario;
+
+    @FXML
+    private MenuItem mmiProdutos;
+
+    @FXML
+    private Menu mmiOfertas;
+	
+    @FXML
+    private Menu mmiCarrinho;
+
+    @FXML
+    private MenuItem mmiVisualizarCarrinho;
+
+    @FXML
+    void menuCadastroUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+	    void menuCarrinho(ActionEvent event) {
+
+    }
+
+    @FXML
+	    void menuEditarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuOfertas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuVisualizarCarrinho(ActionEvent event) {
+
+    }
+	
 }
