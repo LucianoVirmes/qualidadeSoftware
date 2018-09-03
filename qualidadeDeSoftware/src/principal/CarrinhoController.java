3f0029111e9ee8f4d2fcbbf8f326a4fb3acebfab
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-03 13:01:07 -0300

    tela Carrinho

--- qualidadeDeSoftware/src/principal/CarrinhoController.java
+++ qualidadeDeSoftware/src/principal/CarrinhoController.java
@@ -1,5 +1,48 @@
 package principal;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
 public class CarrinhoController {
 
+	@FXML
+	private TableView<?> tblCarrinho;
+
+	@FXML
+	private TableColumn<?, ?> tbcNomeProduto;
+
+	@FXML
+	private TableColumn<?, ?> tbcQuantidade;
+
+	@FXML
+	private TableColumn<?, ?> tbcValor;
+
+	@FXML
+	private Button btnExcluir;
+
+	@FXML
+	private Button btnFinalizarCompra;
+
+	@FXML
+	private Label lblTotal;
+
+	@FXML
+	void excluir(ActionEvent event) {
+
+	}
+
+	@FXML
+	void finalizarCompra(ActionEvent event) {
+
+	}
+
+	@FXML
+	void selecionaProduto(MouseEvent event) {
+
+	}
 }
