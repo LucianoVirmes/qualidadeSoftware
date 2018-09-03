e2c4704933202cdd4816aeb25d45fd5aa6002509
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-03 12:44:41 -0300

    Tela cadastro cliente

---
+++ qualidadeDeSoftware/src/principal/CadastroClienteController.java
@@ -0,0 +1,44 @@
+package principal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CadastroClienteController {
+
+	 @FXML
+	    private TextField tfNome;
+
+	    @FXML
+	    private TextField tfSobrenome;
+
+	    @FXML
+	    private TextField tfCpf;
+
+	    @FXML
+	    private TextField tfEstado;
+
+	    @FXML
+	    private TextField tfCidade;
+
+	    @FXML
+	    private TextField tfBairro;
+
+	    @FXML
+	    private TextField tfRua;
+
+	    @FXML
+	    private TextField tfNumero;
+
+	    @FXML
+	    private TextField tfEmail;
+	    
+	    @FXML
+	    private Button btnCadastrar;
+
+	    @FXML
+	    void cadastrar(ActionEvent event) {
+
+	    }
+}
