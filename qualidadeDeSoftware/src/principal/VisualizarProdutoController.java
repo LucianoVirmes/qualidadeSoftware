fbc9e1fabf749acda0c664a7104a0dd1249fa3a7
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-03 13:58:56 -0300

    tela visualizar produto

--- qualidadeDeSoftware/src/principal/VisualizarProdutoController.java
+++ qualidadeDeSoftware/src/principal/VisualizarProdutoController.java
@@ -1,5 +1,31 @@
 package principal;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class VisualizarProdutoController {
 
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private Label lblModelo;
+
+    @FXML
+    private Label lblValor;
+
+    @FXML
+    private TextField tfQuantidade;
+
+    @FXML
+    private Button btnAdionar;
+
+    @FXML
+    void adicionarCarrinho(ActionEvent event) {
+
+    }
+	
 }
