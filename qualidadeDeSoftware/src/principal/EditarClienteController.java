a9fbe3d078d35923aae82c34ee822e1254937425
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-03 12:48:58 -0300

    tela editar cliente

---
+++ qualidadeDeSoftware/src/principal/EditarClienteController.java
@@ -0,0 +1,44 @@
+package principal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditarClienteController {
+
+	 @FXML
+	    private TextField tfNome;
+
+	    @FXML
+	    private TextField tfSobrenome;
+
+	    @FXML
+	    private TextField tfCpf;
+
+	    @FXML
+	    private TextField tfEstado;
+
+	    @FXML
+	    private TextField tfCidade;
+
+	    @FXML
+	    private TextField tfBairro;
+
+	    @FXML
+	    private TextField tfRua;
+
+	    @FXML
+	    private TextField tfNumero;
+
+	    @FXML
+	    private TextField tfEmail;
+
+	    @FXML
+	    private Button btnEditar;
+
+	    @FXML
+	    void salvar(ActionEvent event) {
+
+	    }
+}
