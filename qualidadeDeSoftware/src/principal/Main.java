69e97b67f06cbb98344ee4a45cb5935e95f5b702
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-04 17:00:14 -0300

    comentario

--- qualidadeDeSoftware/src/principal/Main.java
+++ qualidadeDeSoftware/src/principal/Main.java
@@ -8,13 +8,14 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
-// ver controllers
-
-
 public class Main extends Application {
 	
 	private static Stage stage;
 	
+	/**
+	 * Scenes criados como estaticos e privados 
+	 * para carregados no star
+	 */
 	private static Scene loginScene;
 	private static Scene menuClienteScene;
 	private static Scene menuAdminScene;
@@ -28,26 +29,49 @@ public class Main extends Application {
 		
 		primaryStage.setTitle("ExemploLogin");
 		
+		/**
+		 * carrega a tela de login puxando o fxml 
+		 */
 		BorderPane fxmlLogin = (BorderPane)FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
 		loginScene = new Scene(fxmlLogin, 800,600);
 		loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
+		/**
+		 * carrega a de menu do cliente puxando o fxml
+		 * acessada apos login 
+		 */
 		BorderPane fxmlMenu = (BorderPane)FXMLLoader.load(getClass().getResource("MenuForm.fxml"));
 		menuClienteScene = new Scene(fxmlMenu, 800,600);
 		menuClienteScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
+		/**
+		 * carrega a tela de menu do admin puxando o fxml
+		 * acessada quando o usuario for admin 
+		 */
 		BorderPane fxmlAdminMenu = (BorderPane)FXMLLoader.load(getClass().getResource("AdminMenuForm.fxml"));
 		menuAdminScene  = new Scene(fxmlAdminMenu, 800,600);
 		menuAdminScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
+		/**
+		 * carrega a tela de cadastro do cliente o fxml
+		 * acessada quando botao cadastrar for acionado 
+		 */
 		AnchorPane fxmlCadastroCliente = (AnchorPane)FXMLLoader.load(getClass().getResource("CadastroClienteForm.fxml"));
 		cadastroCliente  = new Scene(fxmlCadastroCliente, 800,600);
 		cadastroCliente.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
+		/**
+		 * carrega a tela finalização de compra puxando o fxml
+		 * acessada ao finalizar a compra em carrinho 
+		 */
 		AnchorPane fxmlFinalizarCompra = (AnchorPane)FXMLLoader.load(getClass().getResource("FinalizarCompraForm.fxml"));
 		finalizarCompra  = new Scene(fxmlFinalizarCompra, 800,600);
 		finalizarCompra.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
+		/**
+		 * carrega a tela de detalhe do produto puxando o fxml
+		 * acessada ao selecionar um produto 
+		 */
 		AnchorPane fxmlVisualizarProduto = (AnchorPane)FXMLLoader.load(getClass().getResource("VisualizarProdutoForm.fxml"));
 		verProduto  = new Scene(fxmlVisualizarProduto, 800,600);
 		verProduto.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -56,8 +80,13 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 	
-	public static void changeScreen(TipoTela tt) {
-		switch (tt) {
+	/**
+	 * metodo que muda a screen recebendo um tipo de tela
+	 * retorna uma scene previamente carregada
+	 * @param tipo
+	 */
+	public static void changeScreen(TipoTela tipo) {
+		switch (tipo) {
 		case LOGIN:
 			stage.setScene(loginScene);
 			break;
--- qualidadeDeSoftware/src/principal/TipoTela.java
+++ qualidadeDeSoftware/src/principal/TipoTela.java
@@ -1,5 +1,10 @@
 package principal;
 
+/**
+ * Criado o enum para escolher o tipo de tela
+ * 
+ * @author TheylorMarmitt
+ */
 public enum TipoTela {
 
 	LOGIN, MENUCLIENTE, CADASTROCLIENTE, MENUADMIN, FINALIZACOMPRA, VERPRODUTO;
