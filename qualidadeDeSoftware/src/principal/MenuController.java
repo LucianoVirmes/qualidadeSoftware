fb1d4ecd6220e43caaaef5dbd705db182229b000
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-06 20:54:18 -0300

    links

--- qualidadeDeSoftware/src/principal/MenuController.java
+++ qualidadeDeSoftware/src/principal/MenuController.java
@@ -1,8 +1,12 @@
 package principal;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class MenuController {
@@ -27,26 +31,61 @@ public class MenuController {
 
 	@FXML
 	void menuEditarUsuario(ActionEvent event) {
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("EditarCliente.fxml"));
+		try {
+			AnchorPane View = (AnchorPane) loader.load();
+			bpPrincipalCliente.setCenter(View);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@FXML
 	void menuHistorico(ActionEvent event) {
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Historico.fxml"));
+		try {
+			AnchorPane View = (AnchorPane) loader.load();
+			bpPrincipalCliente.setCenter(View);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@FXML
 	void menuOferta(ActionEvent event) {
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Oferta.fxml"));
+		try {
+			AnchorPane View = (AnchorPane) loader.load();
+			bpPrincipalCliente.setCenter(View);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@FXML
 	void menuProdutos(ActionEvent event) {
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Produtos.fxml"));
+		try {
+			AnchorPane View = (AnchorPane) loader.load();
+			bpPrincipalCliente.setCenter(View);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@FXML
 	void menuVisualizarCarrinho(ActionEvent event) {
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Carrinho.fxml"));
+		try {
+			AnchorPane View = (AnchorPane) loader.load();
+			bpPrincipalCliente.setCenter(View);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
